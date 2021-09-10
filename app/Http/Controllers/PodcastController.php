a00c38fd9503ccf046591806f1136b7d1b870c05
Davis <antoninodaviscacici@gmail.com>
2021-09-10 03:54:15 +0200

    config storage

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -5,12 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Podcast;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PodcastController extends Controller
 {
     //
+
     function carica(Request $request)
     {
-        dd($request);
+        // dd($request->all());
+        $user = Auth::id();
+        $file = $request->file('file');
+        $descrizione = $request['descrizione'];
+        $name = $request['name'];
+        Podcast::create([
+            "name" => $name,
+            "description" => $descrizione,
+            'path' => Storage::disk('public')->put("/$user", $file),
+            'ext' => $file->getClientOriginalExtention(),
+            'userid' => $user
+        ]);
+
+        return redirect("dashboard");
     }
 }
